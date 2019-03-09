4885820a5ef484258df3eaeeb343a5368b8a865d
Chris Hassan <chriskkhassan@gmail.com>
2019-03-09 19:55:32 +0000

    Refactor: plane.land called in setUp of plane test suite

--- tests/test_plane.py
+++ tests/test_plane.py
@@ -5,12 +5,11 @@ class TestPlane(unittest.TestCase):
 
     def setUp(self):
         self.plane = Plane()
+        self.plane.land()
 
     def test_land_returns_true(self):
-        self.plane.land()
         self.assertEqual(self.plane.grounded, True)
 
     def test_take_off_returns_false(self):
-        self.plane.land()
         self.plane.take_off()
         self.assertEqual(self.plane.grounded, False)
