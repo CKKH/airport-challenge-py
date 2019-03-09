54d3d2e620ba9697532b6ba21e953022cd68984b
Chris Hassan <chriskkhassan@gmail.com>
2019-03-09 01:22:13 +0000

    Feature: airport.land(plane) raises error when hanger at capacity of 1

--- src/airport.py
+++ src/airport.py
@@ -1,12 +1,13 @@
 class Airport:
-    """ Releases and docks bikes """
 
     def __init__(self):
-
         self.hanger = []
 
     def land(self, plane):
-        return self.hanger.append(plane)
+        if len(self.hanger) == 1:
+            return 'Cannot land plane: hanger full'
+        else:
+            return self.hanger.append(plane)
 
     def take_off(self, plane):
         return self.hanger.remove(plane)
--- tests/test_airport.py
+++ tests/test_airport.py
@@ -8,9 +8,14 @@ class TestAirport(unittest.TestCase):
         self.airport = Airport()
         self.plane = Mock(return_value='Plane')
 
-    def test_land_plane_stores_plane_in_array(self):
+    def test_land_plane_stores_plane_in_hanger(self):
         self.airport.land(self.plane)
         self.assertEqual(self.airport.hanger, [self.plane])
+
+    def test_land_plane_raises_error_if_hanger_full(self):
+        self.plane_two = Mock(return_value='Plane Two')
+        self.airport.land(self.plane)
+        self.assertEqual(self.airport.land(self.plane_two), "Cannot land plane: hanger full")
 
     def test_take_off_stores_plane_in_array(self):
         self.airport.land(self.plane)
