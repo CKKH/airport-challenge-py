17124017d088426c7f569b1cc399c0d047c63317
Chris Hassan <chriskkhassan@gmail.com>
2019-03-10 10:52:37 +0000

    Refactor: .take_off(plane) conditions extracted to private method pt.1

--- src/airport.py
+++ src/airport.py
@@ -15,6 +15,9 @@ class Airport:
 
     def take_off(self, plane):
         if plane.grounded == False: return 'Cannot take off: plane not grounded'
+        return self._take_off_if_plane_at_airport(plane)
+
+    def _take_off_if_plane_at_airport(self, plane):
         if plane in self.hanger:
             plane.take_off()
             return self.hanger.remove(plane)
