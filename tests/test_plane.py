301a9b4fec75b2f804f14de12f82dca71322bb0b
Chris Hassan <chriskkhassan@gmail.com>
2019-03-11 16:25:07 +0000

    Refactor: made tests names easier to understand

--- tests/test_plane.py
+++ tests/test_plane.py
@@ -7,16 +7,19 @@ class TestPlane(unittest.TestCase):
         self.plane = Plane()
         self.plane.land()
 
-    def test_land_returns_true(self):
+    def test_land_sets_grounded_status_to_true(self):
         self.assertEqual(self.plane.grounded, True)
 
     def test_land_raises_error_if_plane_grounded(self):
         self.assertRaises(TypeError, self.plane.land)
 
-    def test_take_off_returns_false(self):
+    def test_take_off_sets_grounded_status_to_false(self):
         self.plane.take_off()
         self.assertEqual(self.plane.grounded, False)
 
     def test_take_off_raises_error_if_plane_not_grounded(self):
         self.plane.take_off()
         self.assertRaises(TypeError, self.plane.take_off)
+
+if __name__ == "__main__":
+    unittest.main()
